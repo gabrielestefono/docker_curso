16011ced613aa3ce89fcb1a4239bc6a4cc4119bd
gabrielestefono <gabrielestefono@hotmail.com>
2023-12-08 09:04:32 -0300

    Alteração em tempo real

--- 03_volumes/index.php
+++ 03_volumes/index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Mensagem</title>
+	<title>Mensagem Bind Mount</title>
 </head>
 <body>
 	<h1>Escreva a sua mensagem</h1>
